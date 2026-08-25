8b69b17f29368a1ecef7855c2d5a28cc21ca3b05
pomand1-hub <pomand1@gmail.com>
2026-08-25 17:33:13 +0900

    Add src/dashboard-widget.hpp

---
+++ src/dashboard-widget.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QHash>
+#include <QStringList>
+#include <QWidget>
+
+#include <obs-frontend-api.h>
+
+class QGridLayout;
+class QScrollArea;
+class QTimer;
+class SceneCard;
+
+class DashboardWidget final : public QWidget {
+public:
+    explicit DashboardWidget(QWidget *parent = nullptr);
+    ~DashboardWidget() override;
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    static void frontendEvent(enum obs_frontend_event event, void *data);
+    void refreshScenes();
+    void rebuildGrid();
+    void updateActiveScene();
+    void activateScene(const QString &name);
+    void moveScene(const QString &source, const QString &target);
+    QString settingsKey() const;
+    QStringList loadOrder() const;
+    void saveOrder() const;
+
+    QScrollArea *scroll_ = nullptr;
+    QWidget *gridHost_ = nullptr;
+    QGridLayout *grid_ = nullptr;
+    QTimer *activeTimer_ = nullptr;
+    QStringList order_;
+    QHash<QString, SceneCard *> cards_;
+};
